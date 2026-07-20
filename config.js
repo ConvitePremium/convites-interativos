0f9f77c77d8dfc4f77d0a43c718729d5b86d46e9
ConvitePremium <essenciamusicalofc@gmail.com>
2026-07-20 00:30:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 window.SITE_CONFIG = {
-  whatsapp: "5599999999999",
+  whatsapp: "5588994897535",
   mensagemPadrao: "Olá! Vim pelo site e quero criar um convite interativo personalizado.",
 
   // Cole abaixo os links reais dos três convites publicados no GitHub Pages.
   exemplos: {
-    infantil: "https://SEU-USUARIO.github.io/convite-infantil/",
-    quinzeAnos: "https://SEU-USUARIO.github.io/convite-15-anos/",
-    casamento: "https://SEU-USUARIO.github.io/convite-casamento/"
+    infantil: "https://convitepremium.github.io/convites-premium-Heloisa/?v=2",
+    quinzeAnos: "https://convitepremium.github.io/convites-premium-Nycolle/?v",
+    casamento: "https://convitepremium.github.io/convites-premium-Daianna-e-Neto/?v=2"
   }
 };
